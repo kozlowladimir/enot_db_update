015d55f92b9988cdba42d928899b1eda43072280
kozlowladimir <kozlowladimir@gmail.com>
2020-05-05 21:39:54 +0700

    Внес правки по стилю кода

--- providers.py
+++ providers.py
@@ -1,99 +1,111 @@
 import os
-import pandas as pd
+
 from pathlib import Path
+import pandas as pd
 from loguru import logger
-from abc import ABC, abstractmethod
 import mysql.connector
 
-import config
+from config import connection_params, TABLE_NAME, trash_csv, cols_to_load
 
 
 class RemoteProvider:
+    """Ьазовый класс для удаленных провайдеров."""
     def __init__(self, connection_params):
         logger.debug('__init__')
         self.connection_params = connection_params
         self.connection = None
-        
+        self.cursor = None
+
     def open_connection(self):
+        """Открывает коннект к базе."""
         logger.debug('open_connection')
         self.connection = mysql.connector.connect(**self.connection_params)
-    
+
     def close_connection(self):
+        """Закрывает коннект к базе."""
         logger.debug('close_connection')
         self.connection.close()
-        
+
     def commit(self):
+        """Кммитит изменения в базе."""
         logger.debug('commit')
         self.connection.commit()
-        
+
     def create_cursor(self):
+        """Создает курсор."""
         logger.debug('create_cursor')
         self.cursor = self.connection.cursor()
-        
+
     def close_cursor(self):
+        """Закрывает курсор."""
         logger.debug('close_cursor')
         self.cursor.close()
 
 
 class WriterProvider(RemoteProvider):
+    """Провайдер для записи в базу"""
     def __init__(self, *args, **kwargs):
         logger.debug('__init__')
         super().__init__(*args, **kwargs)
-         
-    def __process_names(self, df: pd.DataFrame) -> pd.DataFrame:
+
+    @staticmethod
+    def process_names(dataframe: pd.DataFrame) -> pd.DataFrame:
         """Функция для процессинга названий товаров."""
         logger.debug('__process_names')
-        df.loc[:, 'name'] = df.loc[:, 'name'].str.replace("'", "")
-        df.loc[:, 'name'] = df.loc[:, 'name'].str.replace('"', '')
-        df.loc[:, 'name'] = df.loc[:, 'name'].str.replace('\\', '')
-        return df
-    
+        dataframe.loc[:, 'name'] = dataframe.loc[:, 'name'].str.replace("'", "")
+        dataframe.loc[:, 'name'] = dataframe.loc[:, 'name'].str.replace('"', '')
+        dataframe.loc[:, 'name'] = dataframe.loc[:, 'name'].str.replace('\\', '')
+        return dataframe
+
     def insert_into_db(self, new_data):
+        """Добавляет в базу новые штрихкоды."""
         logger.debug('insert_into_db')
-        request_begin = "INSERT INTO u0492853_enot_test.{0} values".format(config.table)
-        new_data = self.__process_names(new_data)
-        
+        request_begin = "INSERT INTO {0}.{1} values".format(
+            connection_params['database'],
+            TABLE_NAME,
+        )
+        new_data = self.process_names(new_data)
+
         self.open_connection()
         self.create_cursor()
-        
+
         batch_size = 20000
         logger.debug('batch_size {0}'.format(batch_size))
-        
-        for x in range(new_data.shape[0]//batch_size+1):
+
+        for n_iter in range(new_data.shape[0]//batch_size+1):
             values = [request_begin]
-            for ind, row in new_data[x*batch_size:batch_size*(x+1)].iterrows():
+            for _, row in new_data[n_iter*batch_size:batch_size*(n_iter+1)].iterrows():
                 value = "({1}, \'{2}\', {3}),".format(
-                    request_begin,
                     row['barcode'],
                     row['name'],
-                    row['owner']
+                    row['owner'],
                 )
                 values.append(value)
             values[-1] = values[-1][:-1]
             sql_request = " ".join(values)
-            
+
             self.cursor.execute(sql_request)
-            logger.debug('Step done: {0}/{1}'.format(x+1, new_data.shape[0]//batch_size+1))
-            
+            logger.debug('Step done: {0}/{1}'.format(n_iter+1, new_data.shape[0]//batch_size+1))
+
         self.commit()
         self.close_cursor()
-        self.close_connection()            
-    
+        self.close_connection()
+
 
 class UhhtBarcodeReference():
+    """Провайдер для чтения из репозитрия."""
     def __init__(self, init_path: Path):
         logger.debug('__init__')
         self.init_pass = init_path
         self.all_data = self.__get_table()
 
-    def __load_tables(self, cols: list = []) -> pd.DataFrame:
+    def __load_tables(self, cols: list) -> pd.DataFrame:
         logger.debug('__load_tables')
-        dfs = []
+        dataframes = []
         for path in os.listdir(self.init_pass):
-            if path not in config.trash_csv:
-                
+            if path not in trash_csv:
                 try:
-                    df = pd.read_csv(
+                    dataframe = pd.read_csv(
                         self.init_pass / path,
                         sep='\t',
                         usecols=cols,
@@ -101,39 +113,46 @@ class UhhtBarcodeReference():
                 except pd.errors.ParserError:
                     logger.debug('{0}'.format(path))
                     continue
-                dfs.append(df)             
-        df = pd.concat(dfs)
-        return df
+                dataframes.append(dataframe)
+        dataframe = pd.concat(dataframes)
+        return dataframe
 
     def __get_table(self) -> pd.DataFrame:
         logger.debug('__get_table')
-        df = self.__load_tables(config.cols_to_load)
-        df['owner'] = 100
-        df = df.rename(columns={'UPCEAN': 'barcode', 'Name': 'name'})
-        df = df.drop_duplicates('barcode')
-        return df
-    
+        dataframe = self.__load_tables(cols_to_load)
+        dataframe['owner'] = 100
+        dataframe = dataframe.rename(columns={'UPCEAN': 'barcode', 'Name': 'name'})
+        dataframe = dataframe.drop_duplicates('barcode')
+        return dataframe
+
     def get_barcodes(self):
+        """Возвращает штрихкоды."""
         logger.debug('get_barcodes')
         return set(self.all_data['barcode'].unique())
-    
+
     def get_table_by_barcodes(self, barcodes):
+        """Возвращает таблицу с новыми штрихкодами и названиями товаров."""
         logger.debug('get_table_by_barcodes')
         return self.all_data[self.all_data['barcode'].isin(barcodes)].reset_index(drop=True)
 
 
 class UhhtBarcodeReferenceRemote(RemoteProvider):
+    """Удаленный провайдер для чтения из базы."""
     def __init__(self, *args, **kwargs):
         logger.debug('__init__')
-        super().__init__(*args, **kwargs) 
-        
-    def get_barcodes(self):
-        logger.debug('get_barcodes')
-        self.open_connection()
+        super().__init__(*args, **kwargs)
 
-        df = pd.read_sql(
-            'select barcode from u0492853_enot_test.{0}'.format(config.table),
-            con=self.connection
+    def get_barcodes(self):
+        """Получает щтрихкода из базы"""
+        logger.debug('get_barcodes')
+
+        sql_request = 'select barcode from {0}.{1}'.format(
+            connection_params['database'],
+            TABLE_NAME,
         )
+
+        logger.debug('{0}'.format(sql_request))
+        self.open_connection()
+        dataframe = pd.read_sql(sql_request, con=self.connection)
         self.close_connection()
-        return set(df['barcode'].unique())
+        return set(dataframe['barcode'].unique())
--- script.py
+++ script.py
@@ -1,49 +1,48 @@
-import os
-import mysql.connector
-
-import pandas as pd
-from loguru import logger
 from pathlib import Path
+from loguru import logger
 
-import config
+from config import PATH_TO_REPOSITORY_DATA, connection_params
 from providers import UhhtBarcodeReference, UhhtBarcodeReferenceRemote, WriterProvider
 
 
 def main():
-    with open('current_commit_hash', 'r') as f:
-        current_commits_hash = f.read()
+    """Главная функция для апдейта базы"""
+    with open('current_commit_hash', 'r') as file:
+        current_commits_hash = file.read()
 
     try:
-        with open('last_commit_hash', 'r') as f:
-            last_commits_hash = f.read()
+        with open('last_commit_hash', 'r') as file:
+            last_commits_hash = file.read()
     except FileNotFoundError:
         last_commits_hash = ''
 
     if current_commits_hash == last_commits_hash:
         logger.debug('Nothing to update. Commits hashes are equal.')
         return 0
-    
-    init_path = Path(config.path_to_repositiry_data)
-    
-    uhhtBarcodeReference = UhhtBarcodeReference(init_path)
-    uhhtBarcodeReferenceRemote = UhhtBarcodeReferenceRemote(config.connection_params)
-    
-    local_barcodes = uhhtBarcodeReference.get_barcodes()
-    remote_barcodes = uhhtBarcodeReferenceRemote.get_barcodes()
-    
+
+    init_path = Path(PATH_TO_REPOSITORY_DATA)
+
+    uhht_barcode_reference = UhhtBarcodeReference(init_path)
+    uhht_barcode_reference_remote = UhhtBarcodeReferenceRemote(connection_params)
+
+    local_barcodes = uhht_barcode_reference.get_barcodes()
+    remote_barcodes = uhht_barcode_reference_remote.get_barcodes()
+
     new_barcodes = local_barcodes - remote_barcodes
     if len(new_barcodes) > 0:
         logger.debug('There are {0} new barcodes'.format(len(new_barcodes)))
-        
-        new_table = uhhtBarcodeReference.get_table_by_barcodes(new_barcodes)
-        writerProvider = WriterProvider(config.connection_params)
-        writerProvider.insert_into_db(new_table)
-        
+
+        new_table = uhht_barcode_reference.get_table_by_barcodes(new_barcodes)
+        writer_provider = WriterProvider(connection_params)
+        writer_provider.insert_into_db(new_table)
     else:
         logger.debug('There are no new barcodes')
-    
-    with open('last_commit_hash', 'w') as f:
-        f.write("{0}".format(current_commits_hash))
+
+    with open('last_commit_hash', 'w') as file:
+        file.write("{0}".format(current_commits_hash))
+
+    return 0
+
 
 if __name__ == '__main__':
     main()
